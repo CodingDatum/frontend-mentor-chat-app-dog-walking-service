97b88f931e50b64788ca712fd2a95b7e3a473a21
CodingDatum <73291844+CodingDatum@users.noreply.github.com>
2021-04-11 11:47:31 -0400

    Add files via upload
    added sound effects, text-effects, and the missing content

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+let textAlert = new Audio ("sounds/text-alert.mp3");
+
+let textAlert2 = new Audio("sounds/text-alert-2.mp3")
+
+
 let textMessages = document.querySelectorAll(".text");
 
 for ( let i = 0 ; i < textMessages.length; i++){
 
     setTimeout( function(){
 
-        textMessages[i].style.visibility = "visible"
+        let text = textMessages[i]
+
+        console.log(text.classList)
+
+        if(text.classList.contains("right-side-text") || text.classList.contains("text-images")){
+
+            textAlert.play();
+
+        }else{
+
+            textAlert2.play();
+
+        }
+
+
+        text.classList.add("animate-text")
+
 
     },i*2000)
 
